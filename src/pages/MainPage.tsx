1559add127baf90add85e996da478453d6ecee1c
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-02-28 01:18:35 +0900

    <update: 파일 구조 정리 및 메인레이아웃 적용>

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,40 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import styled, { keyframes } from 'styled-components';
 import Wrapper from '../components/common/Wrapper';
-import AboutMe from '../components/Category/AboutMe';
 
 const MainPage = () => {
 
   return (
-    <MainLayOut>
-      <EffectAnimation>
-        <AboutMe />
-      </EffectAnimation>
-    </MainLayOut>
+    <div>
+      MainPage
+    </div>
   )
 };
-
-const MainLayOut = styled.main`
-  width: 100%;
-  height: 100vh;
-  background-color: #212226;
-  position: relative;
-`;
-
-const EffectAnimation = styled.div`
-  width: 1320px;
-  height: 100%;
-  color: #d4b681;
-  font-family: "GongGothicMedium";
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 0px auto;
-
-  @media screen and (max-width: 1320px) {
-    width: 96%;
-  }
-`;
 
 export default MainPage;
