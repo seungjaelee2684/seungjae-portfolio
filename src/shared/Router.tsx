e713ca889301af1b6fc5445fbc557290fede6d4f
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-02-28 17:45:19 +0900

    <feat: 메인페이지 css마크업>

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -4,6 +4,7 @@ import MainPage from '../pages/MainPage';
 import { AnimatePresence } from 'framer-motion';
 import AboutUsPage from '../pages/AboutMePage';
 import MainLayout from '../components/common/MainLayout';
+import StartPage from '../pages/StartPage';
 
 const Router = () => {
   return (
@@ -11,7 +12,8 @@ const Router = () => {
         <AnimatePresence>
             <Routes>
               <Route element={<MainLayout />}>
-                <Route path='/' element={<MainPage />} />
+                <Route path='/' element={<StartPage />} />
+                <Route path='/main' element={<MainPage />} />
                 <Route path='/about' element={<AboutUsPage />} />
               </Route>
             </Routes>
