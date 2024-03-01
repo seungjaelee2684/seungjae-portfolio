ed3ce62446ec86079ca0b7a424670a221d83ec5e
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-03-02 02:23:39 +0900

    <feat: about us 페이지 상태값 자식 컴포넌트에 props>

--- src/components/AboutMePage/InfoModal.tsx
+++ src/components/AboutMePage/InfoModal.tsx
@@ -1,23 +1,44 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const InfoModal = () => {
+interface InfoModalProps {
+  statusModal: {
+    information: boolean,
+    state: number | undefined
+  };
+};
+
+const InfoModal : React.FC<InfoModalProps> = ({ statusModal }) => {
+
+  const { information, state } = statusModal;
+
   return (
     <ModalContainer>
-        InfoModal
+      <ModalInWrapper>
+        {state}
+      </ModalInWrapper>
     </ModalContainer>
   )
 };
 
 const ModalContainer = styled.div`
-    width: 300px;
-    height: 200px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    top: 0;
-    left: 0;
+  width: 300px;
+  height: 400px;
+  position: absolute;
+  top: 50px;
+  left: 0;
+  overflow: hidden;
+  z-index: 13;
+  border: 1px solid #208bf0ab;
+`;
+
+const ModalInWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(to top, #3b80c0ef, #10395fee);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default InfoModal;
