629cd0f3bcc654fec066e7213060af4e24f57d8f
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-04-04 23:57:44 +0900

    <update: 가이드 내용 수정>

--- src/components/common/GuideAnimation.tsx
+++ src/components/common/GuideAnimation.tsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import styled, { keyframes } from 'styled-components';
 import { GuideFadeIn } from '../../styles/guide';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { guideOpen } from '../../store/modules/guide';
 import { LuArrowBigDownDash } from "react-icons/lu";
 import { guideContent } from '../../utils/GuideContent';
+import { RootState } from '../../store/config/configureStore';
 
 const GuideAnimation = () => {
 
   const guide = localStorage.getItem("guide");
   const dispatch = useDispatch();
-
+  const isMobile = useSelector((state: RootState) => state.isMobile);
   const guideList = guideContent?.filter((item) => item?.id === guide);
   const guideInfo = guideList ? guideList[0] : null;
 
   return (
     <GuideContainer
+      path={guide}
       onClick={() => {
         dispatch(guideOpen(false));
       }}>
@@ -60,17 +62,17 @@ const ModalArrow = keyframes`
   }
 `;
 
-const GuideContainer = styled.div`
+const GuideContainer = styled.div<{ path: string | null }>`
   color: #d4b681;
   position: absolute;
-  top: -20%;
-  left: 0;
+  top: ${(props) => (props.path === "dungeon") ? "-150px" : "-100px"};
+  left: ${(props) => (props.path === "dungeon") ? "42%" : "10%"};
   z-index: 23;
   font-family: "GongGothicMedium";
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: start;
   gap: 10px;
 `;
 
@@ -82,20 +84,32 @@ const GuideBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 10px;
-  font-size: 12px;
+  gap: 16px;
+  font-size: 16px;
   font-weight: 400;
+  line-height: 150%;
   background-color: #FFFFFF;
   border-radius: 8px;
   animation: ${ModalFadeIn} 0.7s forwards 0.3s;
-  padding: 20px 10px;
+  padding: 30px 16px;
   white-space: pre-line;
+
+  @media screen and (max-width: 500px) {
+    font-size: 10px;
+    line-height: 120%;
+    padding: 16px 8px;
+  }
 `;
 
 const Arrow = styled.div`
+  margin-left: 30px;
   font-size: 50px;
   opacity: 0;
   animation: ${ModalArrow} 1s infinite forwards 1s;
+
+  @media screen and (max-width: 500px) {
+    font-size: 30px;
+  }
 `;
 
 export default GuideAnimation;
