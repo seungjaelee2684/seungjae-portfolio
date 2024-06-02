e28f588d93e1ca0bb1f45e688314a673a0ff5cf9
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-06-02 11:07:13 +0900

    <Feat: About Us 페이지 마크업>

--- src/pages/SitePage.tsx
+++ src/pages/SitePage.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 import WebHeader from '../components/SitePage/WebHeader';
 import Banner from '../components/SitePage/Banner';
 import About from '../components/SitePage/About';
-import Background from '../assets/images/main_background.webp';
+import Background1 from '../assets/images/main_background.webp';
+import Background2 from '../assets/images/start_back.jpg';
 
 const SitePage = () => {
 
@@ -35,20 +36,21 @@ const SitePage = () => {
   return (
     <MainLayout>
       <WebHeader headerRef={headerRef} />
-      <MainBackground src={Background} />
+      <MainBackground src={Background1} />
       <MainContainer>
         <About />
       </MainContainer>
+      <MainBackground src={Background2} />
     </MainLayout>
   )
 };
 
 const MainLayout = styled.article`
   width: 100%;
-  height: 100vh;
   position: relative;
   font-family: "TTLaundryGothicB";
   font-size: 400;
+  background-color: #222020;
 `;
 
 const MainBackground = styled.div<{ src: string }>`
