9edf9076d58da69e88b9790214cb49dd27546556
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-06-14 11:32:00 +0900

    <Feat: Skill 컴포넌트 마크업 작업>

---
+++ src/components/SitePage/Skill.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components';
+import Picture from '../../assets/images/picture.webp';
+
+const Skill = () => {
+  return (
+    <SkillContainer>
+      
+    </SkillContainer>
+  )
+};
+
+const SkillContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  padding: 30px 0px;
+  color: #222020;
+  box-sizing: border-box;
+`;
+
+export default Skill;
