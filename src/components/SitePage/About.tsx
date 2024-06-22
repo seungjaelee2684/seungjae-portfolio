b0e62e066a9d8ae073f245249750e20a58ba327b
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-06-22 22:30:27 +0900

    <Feat: About 컴포넌트 이미지 추가 및 텍스트 추가>

--- src/components/SitePage/About.tsx
+++ src/components/SitePage/About.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import styled, { keyframes } from 'styled-components';
-import Image from '../../assets/images/picture.webp';
+import Image1 from '../../assets/images/about_1.jpg';
+import Image2 from '../../assets/images/about_2.jpg';
 
 interface AboutProps {
     
@@ -12,9 +13,26 @@ const About: React.FC<AboutProps> = () => {
         <AboutMeContainer id="about us">
             <MainTitle>About Us</MainTitle>
             <AboutContentWrapper>
-                <AboutCardBox>
-                    
-                </AboutCardBox>
+                <AboutLaneContainer>
+                    <LeftWrapper>
+                        <LaneTitle>끊임없는 끈기</LaneTitle>
+                        <LaneContent>
+                            아우
+                        </LaneContent>
+                        <Button>자세히보기</Button>
+                    </LeftWrapper>
+                    <CardImage src={Image1} alt='about 이미지1' />
+                </AboutLaneContainer>
+                <AboutLaneContainer>
+                    <CardImage src={Image2} alt='about 이미지1' />
+                    <LeftWrapper>
+                        <LaneTitle>끊임없는 끈기</LaneTitle>
+                        <LaneContent>
+                            아우
+                        </LaneContent>
+                        <Button>자세히보기</Button>
+                    </LeftWrapper>
+                </AboutLaneContainer>
             </AboutContentWrapper>
         </AboutMeContainer>
     )
@@ -41,19 +59,50 @@ const MainTitle = styled.h1`
 
 const AboutContentWrapper = styled.div`
     width: 100%;
+    height: 100%;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     gap: 40px;
 `;
 
-const AboutCardBox = styled.div`
+const AboutLaneContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 150px;
+`;
+
+const LeftWrapper = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: 16px;
+    gap: 10px;
+`;
+
+const LaneTitle = styled.strong`
+    font-size: 24px;
+    line-height: normal;
+`;
+
+const LaneContent = styled.p`
+    font-size: 18px;
+    line-height: 120%;
+`;
+
+const CardImage = styled.img`
+    min-width: 500px;
+    height: 300px;
+    object-fit: cover;
+`;
+
+const Button = styled.button`
+    width: 140px;
+    height: 48px;
 `;
 
 export default About;
