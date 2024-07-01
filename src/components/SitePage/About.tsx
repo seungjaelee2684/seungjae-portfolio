ec001bbde24ef2337d6a02668ce6717beb45e0dc
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-07-02 01:36:16 +0900

    <Feat: about 컴포넌트 상세정보 모달 기능 구현>

--- src/components/SitePage/About.tsx
+++ src/components/SitePage/About.tsx
@@ -4,13 +4,51 @@ import Image1 from '../../assets/images/about_1.jpg';
 import Image2 from '../../assets/images/about_2.jpg';
 
 interface AboutProps {
-    
+
 };
 
 const About: React.FC<AboutProps> = () => {
 
+    const backgroundRef = useRef<HTMLDivElement>(null);
+    const modalRef = useRef<HTMLDivElement>(null);
+    const [aboutOpen, setAboutOpen] = useState<{
+        state: string,
+        isopen: boolean
+    }>({
+        state: '',
+        isopen: false
+    });
+    const { state, isopen } = aboutOpen;
+
+    const onClickModalOpenHandler = (st: string) => {
+        setAboutOpen(prev => ({ ...prev, state: st, isopen: true }))
+    };
+
+    const onClickModalCloseHandler = () => {
+        setAboutOpen(prev => ({ ...prev, state: '', isopen: false }));
+    };
+
+    useEffect(() => {
+        if (!modalRef.current || !backgroundRef.current) return;
+        if (isopen) {
+            backgroundRef.current.style.visibility = 'visible';
+            modalRef.current.style.visibility = 'visible';
+            modalRef.current.style.transform = 'scale(1)';
+
+        } else {
+            backgroundRef.current.style.visibility = 'hidden';
+            modalRef.current.style.visibility = 'hidden';
+            modalRef.current.style.transform = 'scale(0)';
+        };
+    }, [aboutOpen]);
+
     return (
         <AboutMeContainer id="about us">
+            <ModalBackground ref={backgroundRef} onClick={onClickModalCloseHandler}>
+                <ModalContainer ref={modalRef}>
+
+                </ModalContainer>
+            </ModalBackground>
             <MainTitle>About Us</MainTitle>
             <AboutContentWrapper>
                 <AboutLaneContainer>
@@ -19,7 +57,7 @@ const About: React.FC<AboutProps> = () => {
                         <LaneContent>
                             자신의 한계를 뛰어넘는 원동력!
                         </LaneContent>
-                        <Button>자세히보기</Button>
+                        <Button onClick={() => onClickModalOpenHandler('끈기')}>자세히보기</Button>
                     </LeftWrapper>
                     <CardImage src={Image1} alt='about 이미지1' />
                 </AboutLaneContainer>
@@ -30,7 +68,7 @@ const About: React.FC<AboutProps> = () => {
                         <LaneContent>
                             프로젝트의 성공을 좌지우지할 수 있는 힘!
                         </LaneContent>
-                        <Button>자세히보기</Button>
+                        <Button onClick={() => onClickModalOpenHandler('책임감')}>자세히보기</Button>
                     </LeftWrapper>
                 </AboutLaneContainer>
             </AboutContentWrapper>
@@ -127,6 +165,38 @@ const Button = styled.button`
     &:hover {
         background-color: #6a64ab;
     }
+`;
+
+const ModalBackground = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #22202050;
+    backdrop-filter: blur(3px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 25;
+    visibility: hidden;
+`;
+
+const ModalContainer = styled.div`
+    width: 600px;
+    height: 700px;
+    box-sizing: border-box;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    background-color: #FEFEFE;
+    box-shadow: #222020 0px 2px 2px 0px;
+    visibility: hidden;
+    transition: all 0.3s;
+    transform: scale(0);
 `;
 
 export default About;
