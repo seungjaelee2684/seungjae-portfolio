41d5df56563d576655258e777b7e9f8d4f680529
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-11-29 04:05:28 +0900

    <Feat: Supabase 연결>

--- src/pages/SitePage.tsx
+++ src/pages/SitePage.tsx
@@ -1,13 +1,64 @@
 import React, { useEffect, useRef, useState } from 'react'
 import styled, { keyframes } from 'styled-components';
 import SiteHeader from '../components/SitePage/SiteHeader';
+import SideTap from '../components/SitePage/SideTap';
+import { supabase } from '../utils/Supabase';
+import Profile from '../components/SitePage/Profile';
 
 const SitePage = () => {
+
+  const [blogData, setBlogData] = useState<any>(null);
+  console.log(blogData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('projects')
+          .select('*');
+
+          if (error) {
+            throw error;
+          };
+
+          setBlogData(data);
+      } catch (error) {
+        console.error("Error fetching paginated data from Supabase: ", error);
+      };
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
+    <SiteContainer>
       <SiteHeader />
-    </div>
+      <Profile />
+      <SiteWrapper>
+        <SideTap />
+      </SiteWrapper>
+    </SiteContainer>
   )
 };
+
+const SiteContainer = styled.div`
+  width: 900px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  margin: 120px auto 0px auto;
+
+  @media screen and (max-width: 900px) {
+    width: 94%;
+  }
+`;
+
+const SiteWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: start;
+  align-items: start;
+`;
 
 export default SitePage;
