fc2e6e2e5324bb213120f1c1eed02e605655adb0
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-12-06 16:54:51 +0900

    <Refactor: 메일 전송 페이지 수정>

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -87,7 +87,7 @@ const StartPage = () => {
               onMouseOver={() => setInfo(4)}
               onMouseLeave={() => setInfo(undefined)}
               onClick={() => {
-                navigate("/jaelog/contact");
+                navigate("/contact");
               }}>
               <HoverButton />
               <ButtonText>
