53b974516a6605c9803dba73dd4dd9dd7c65467d
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-12-10 02:56:17 +0900

    <Feat: 이력서 페이지 마크업 작업>

--- src/styles/colorToken.ts
+++ src/styles/colorToken.ts
@@ -1,4 +1,6 @@
 export const commonBgColor = ['#ffffff', '#222020'];
 export const commonTextColor = ['#222020', '#ffffff'];
-export const textLight = ['#575757', '#c0c0c0'];
-export const textMedium = ['#302e2e', '#ffffff']
+export const textLight = ['#838383', '#c0c0c0'];
+export const textMedium = ['#302e2e', '#ffffff'];
+export const lineLight = ['#cecece', '#c0c0c0'];
+export const textLightBlue = ['#67738e', '#b7c6e7'];
