a24690601f48cc5eb1920d0137834085d6e924bf
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-12-16 23:34:56 +0900

    <Refactor: 로딩 컴포넌트 수정 및 커스텀>

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,33 +1,55 @@
 import React from 'react'
-import LoadingGif from '../../assets/images/loading.gif';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const Loading = () => {
   return (
     <LoadingContainer>
-        <LoadingImg src={LoadingGif} alt="Loading..." />
+      <LoadingBox $delay='0.1s' />
+      <LoadingBox $delay='0.4s' />
+      <LoadingBox $delay='0.7s' />
     </LoadingContainer>
   )
 };
 
-const LoadingContainer = styled.div`
-    width: 100%;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 25;
-    backdrop-filter: blur(3px);
-    background-color: #000000;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+const LoadingAnimation = keyframes`
+  0% {
+    background-color: #0c5ba5;
+    transform: scale(1);
+  }
+
+  50% {
+    background-color: #177edf;
+    transform: scale(1.5);
+  }
+
+  100% {
+    background-color: #0c5ba5;
+    transform: scale(1);
+  }
 `;
 
-const LoadingImg = styled.img`
-    width: 300px;
-    height: auto;
-    object-fit: cover;
+const LoadingContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 25;
+  backdrop-filter: blur(3px);
+  background-color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+const LoadingBox = styled.div<{ $delay: string }>`
+  width: 18px;
+  height: 18px;
+  border-radius: 100%;
+  background-color: #177edf;
+  animation: ${LoadingAnimation} 1.5s linear infinite forwards;
+  animation-delay: ${(props) => props.$delay};
 `;
 
 export default Loading;
