9511af3c7fde814ae380f177da44f23f0212cf87
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2025-02-11 01:07:13 +0900

    <Feat: 글자 SVG적용>

---
+++ src/components/MainPage/FullLayout.tsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from 'react'
+import styled, { keyframes } from 'styled-components';
+import SiteHeader from '../SitePage/SiteHeader';
+import Profile from '../SitePage/Profile';
+import { IoGameController } from "react-icons/io5";
+import { Outlet } from 'react-router-dom';
+import { commonBgColor, commonTextColor } from '../../styles/colorToken';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/config/configureStore';
+import { LiaArrowUpSolid } from "react-icons/lia";
+
+const SiteLayout = () => {
+
+  const path = window.location.pathname;
+  const theme = useSelector((state: RootState) => state.darkMode);
+  const font = useSelector((state: RootState) => state.fontSize);
+  const [scroll, setScroll] = useState<number>(window.scrollY);
+
+  const onClickTopHandler = (e: any) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    const scrollEvent = () => {
+      const y = window.scrollY;
+      setScroll(y);
+    };
+
+    window.addEventListener('scroll', scrollEvent);
+
+    return () => {
+      window.removeEventListener('scroll', scrollEvent);
+    }
+  }, []);
+
+  return (
+    <SiteContainer $font={font}>
+      <SiteHeader />
+      {(path === "/jaelog" || path === "/jaelog/resume") && <Profile />}
+      <Outlet />
+      <FloatingButton
+        $color={commonTextColor[theme]}
+        onClick={onClickTopHandler}
+        $display={scroll > 200}>
+        <LiaArrowUpSolid />
+      </FloatingButton>
+      <GameModeButton
+        title='게임모드로 이동'
+        href='/gamemode'>
+        <IoGameController />
+      </GameModeButton>
+    </SiteContainer>
+  )
+};
+
+const SiteContainer = styled.article<{ $font: string }>`
+  width: 1140px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  margin: 0px auto;
+  padding-top: 100px;
+  font-family: "SUIT_Regular";
+  font-size: ${(props) => props.$font};
+
+  @media screen and (max-width: 1140px) {
+    width: 94%;
+    padding-top: 70px;
+  }
+`;
+
+const FloatingButton = styled.button<{ $color: string, $display: boolean }>`
+  width: 50px;
+  height: 50px;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 20;
+  outline: none;
+  border: none;
+  background-color: ${(props) => props.$color}47;
+  color: #ffffff;
+  border-radius: 100%;
+  transition: all 0.3s;
+  font-size: 20px;
+  visibility: ${(props) => props.$display ? 'visible' : 'hidden'};
+  opacity: ${(props) => props.$display ? '1' : '0'};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  -webkit-transition: all 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.$color}80;
+  }
+
+  @media screen and (max-width: 1140px) {
+    width: 30px;
+    height: 30px;
+    font-size: 16px;
+    bottom: 10px;
+    right: 10px;
+  }
+`;
+
+const GameModeButton = styled.a`
+  width: 50px;
+  height: 50px;
+  position: fixed;
+  bottom: 78px;
+  right: 20px;
+  z-index: 20;
+  outline: none;
+  border: none;
+  background-color: #ee6e6e;
+  box-shadow: 2px 2px 4px 0px #560c0c80;
+  color: #ffffff;
+  border-radius: 100%;
+  transition: all 0.3s;
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #cb4747;
+  }
+
+  @media screen and (max-width: 1140px) {
+    width: 30px;
+    height: 30px;
+    font-size: 16px;
+    bottom: 48px;
+    right: 10px;
+  }
+`;
+
+export default SiteLayout;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,23 +1,62 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Header from '../components/common/Header';
 
 const MainPage = () => {
-  return (
-    <MainPageLayout>
-        <MainBanner>
-            <BannerContent>
-                <IntroText>
-                    방문해주셔서 감사합니다
-                </IntroText>
-                <IntroText>
-                    프론트엔드 개발자 이승재입니다
-                </IntroText>
-            </BannerContent>
-        </MainBanner>
-    </MainPageLayout>
-  )
+    return (
+        <MainPageLayout>
+            <MainBanner>
+                <BannerContent>
+                    <IntroText>
+                        방문해주셔서 감사합니다
+                    </IntroText>
+                    <IntroText>
+                        프론트엔드 개발자 <StrongText>이승재</StrongText>입니다
+                    </IntroText>
+                </BannerContent>
+                {/* <svg viewBox="0 0 72 72" className="css-0 e1t0g76a0">
+                    <g transform="translate(-64 -531)">
+                        <rect width="72" height="72" transform="translate(64 531)" fill="none">
+                        </rect>
+                        <Path d="M211.862,1168.227h43.684s0,39.292-36.029,46.187"
+                            transform="translate(-133 -624)">
+                        </Path>
+                    </g>
+                </svg> */}
+                <svg viewBox="0 0 72 72" className="css-0 e1t0g76a0">
+                    <g transform="translate(-64 -531)">
+                        <rect width="72" height="72" transform="translate(64 531)" fill="none"></rect>
+                        <Path d="M 215 1165 V 1210 H 260"
+                            transform="translate(-133 -624)">
+                        </Path>
+                    </g>
+                </svg>
+            </MainBanner>
+        </MainPageLayout>
+    )
 };
+
+const Draw = keyframes`
+    0% {
+        stroke-dashoffset: 107.284;
+        stroke-width: 16;
+    }
+
+    50% {
+        stroke-dashoffset: 0;
+        stroke-width: 16;
+    }
+
+    90% {
+        stroke-dashoffset: 90;
+        stroke-width: 8;
+    }
+
+    100% {
+        stroke-dashoffset: 107.284;
+        stroke-width: 8;
+    }
+`;
 
 const MainPageLayout = styled.div`
     width: 100%;
@@ -50,6 +89,28 @@ const IntroText = styled.p`
     font-size: 2.5em;
     font-weight: 700;
     text-align: start;
+`;
+
+const StrongText = styled.strong`
+    color: #ee6e6e;
+`;
+
+const Svg = styled.svg`
+    width: 500px;
+    height: 200px;
+    background-color: #f9f9f9;
+`;
+
+const Path = styled.path`
+    fill: none;
+    stroke: #D8E2E9;
+    stroke-linecap: square;
+    stroke-miterlimit: 10;
+    opacity: 1;
+    stroke-width: 16;
+    stroke-dasharray: 0;
+    stroke-dashoffset: 0;
+    /* animation: ${Draw} 2s linear forwards; */
 `;
 
 export default MainPage;
